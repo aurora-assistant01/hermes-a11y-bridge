ddbdb9d127551b9e787e55e9807b8c39a7b0e259
aurora-assistant01 <auroraassistant01@gmail.com>
2026-07-02 00:42:11 +0800

    ci: harden V2 embedding, remove legacy kotlin/ sources, standardize workflow

--- android/app/src/main/java/com/hermes/a11ygo/MainActivity.kt
+++ android/app/src/main/java/com/hermes/a11ygo/MainActivity.kt
@@ -7,7 +7,7 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     private val CHANNEL = "com.hermes.a11y/bridge"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
